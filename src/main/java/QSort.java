63fb0074243cb56f9aedfe4757ee04bedb6f1144
Andrey Naumenko <indy256@gmail.com>
2014-04-23 15:39:20 +0300

    update

---
+++ src/main/java/QSort.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class QSort {
+
+	public static void qSort(int[] a, int low, int high) {
+	}
+
+	public static void main(String[] args) {
+		Random rnd = new Random(1);
+		for (int step = 0; step < 10_000; step++) {
+			int n = rnd.nextInt(100);
+			int[] a = new int[n];
+			for (int i = 0; i < n; i++) {
+				a[i] = rnd.nextInt();
+			}
+			int[] b = a.clone();
+			qSort(b, 0, n - 1);
+			Arrays.sort(a);
+			if (!Arrays.equals(a, b))
+				throw new RuntimeException();
+		}
+	}
+}
